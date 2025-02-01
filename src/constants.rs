70470aaf87f958be198ba6f9a0d58d16653fcd90
Mr Speaker <mrspeaker@gmail.com>
2025-02-01 12:36:03 +0800

    faster

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 pub const STONE_RADIUS: f32 = 10.0;
 pub const STONE_DAMPENING: f32 = 0.15;
-pub const STONE_MAX_VEL: f32 = 250.0;
+pub const STONE_MAX_VEL: f32 = 400.0;
 
 pub const SHEET_LENGTH: f32 = 2000.0;
 pub const SHEET_WIDTH: f32 = 400.0;
